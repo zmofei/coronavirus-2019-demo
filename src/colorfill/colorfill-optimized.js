864c3dd783a7fbf389673f5f3cb77f847bb80aad
zmofei <13761509829@163.com>
2020-03-03 20:35:20 +0800

    add color fill layer

---
+++ src/colorfill/colorfill-optimized.js
@@ -0,0 +1,125 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoiemh1d2VubG9uZyIsImEiOiJjazdhNGF6dzIwd3V0M21zNHU1ejZ1a3Q4In0.VkUeaPhu-uMepNBOMc_UdA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10', // stylesheet location
+    center: [33.54, 102.01],
+    zoom: 3,
+    hash: true,
+});
+
+map.on('load', () => {
+    // Geojson 文件
+    map.addSource('coronavirus', {
+        'type': 'geojson',
+        'data': '../data/all.geojson'
+    });
+
+    // 地区多边形
+    map.addSource('area-polygon', {
+        'type': 'vector',
+        'url': 'mapbox://zhuwenlong.bpyitk7f'
+    });
+
+    map.addLayer({
+        'id': 'area-fill',
+        'source': 'area-polygon',
+        'source-layer': 'cn_sheng_polygon',
+        'type': 'fill',
+        "paint": {
+            "fill-color": [
+                "match",
+                [
+                    "get",
+                    "name"
+                ],
+                "湖北",
+                "rgba(204,30,29,1)",
+                "广东",
+                "rgba(239,63,63,1)",
+                "河南",
+                "rgba(239,63,63,1)",
+                "浙江",
+                "rgba(239,63,63,1)",
+                "湖南",
+                "rgba(240,64,64,1)",
+                "安徽",
+                "rgba(240,64,64,1)",
+                "江西",
+                "rgba(241,69,65,1)",
+                "山东",
+                "rgba(247,86,71,1)",
+                "江苏",
+                "rgba(251,98,74,1)",
+                "重庆",
+                "rgba(252,103,75,1)",
+                "四川",
+                "rgba(253,107,76,1)",
+                "黑龙江",
+                "rgba(255,114,80,1)",
+                "北京",
+                "rgba(255,122,86,1)",
+                "上海",
+                "rgba(255,134,96,1)",
+                "河北",
+                "rgba(255,137,99,1)",
+                "福建",
+                "rgba(255,139,101,1)",
+                "广西",
+                "rgba(255,145,106,1)",
+                "陕西",
+                "rgba(255,149,109,1)",
+                "云南",
+                "rgba(255,157,116,1)",
+                "海南",
+                "rgba(255,160,119,1)",
+                "贵州",
+                "rgba(255,162,121,1)",
+                "天津",
+                "rgba(255,162,121,1)",
+                "山西",
+                "rgba(255,162,121,1)",
+                "辽宁",
+                "rgba(255,166,124,1)",
+                "香港",
+                "rgba(255,168,126,1)",
+                "吉林",
+                "rgba(255,168,126,1)",
+                "甘肃",
+                "rgba(255,168,126,1)",
+                "新疆",
+                "rgba(255,175,132,1)",
+                "内蒙古",
+                "rgba(255,175,132,1)",
+                "宁夏",
+                "rgba(255,175,132,1)",
+                "台湾",
+                "rgba(255,191,149,1)",
+                "青海",
+                "rgba(255,199,157,1)",
+                "澳门",
+                "rgba(255,199,157,1)",
+                "西藏",
+                "rgba(255,208,166,1)",
+                "black"
+            ]
+        }
+    })
+
+    map.addLayer({
+        'id': 'area-outline',
+        'source': 'area-polygon',
+        'source-layer': 'cn_sheng_polygon',
+        'type': 'line',
+        "paint": {
+            "line-color": 'rgba(0,0,0,0.3)',
+        }
+    })
+
+
+    map.on('click', (e) => {
+        const features = map.queryRenderedFeatures(e.point);
+        if (features.length > 0) {
+            console.log(features);
+        }
+    });
+});
--- src/colorfill/colorfill.js
+++ src/colorfill/colorfill.js
@@ -2,84 +2,43 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoiemh1d2VubG9uZyIsImEiOiJjazdhNGF6dzIwd3V0M21zN
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v10', // stylesheet location
-    center: [74.2, 10.6],
-    zoom: 1.5,
+    center: [33.54, 102.01],
+    zoom: 3,
     hash: true,
 });
 
 map.on('load', () => {
+    // Geojson 文件
     map.addSource('coronavirus', {
         'type': 'geojson',
         'data': '../data/all.geojson'
     });
 
-    map.addLayer(
-        {
-            'id': 'coronavirus-heat',
-            'type': 'heatmap',
-            'source': 'coronavirus',
-            'paint': {
-                // 热力权重，适用于集合图
-                // Mapbox Expression https://docs.mapbox.com/mapbox-gl-js/style-spec/expressions/
-                'heatmap-weight': 1 || [
-                    'interpolate',
-                    ['linear'],
-                    ['get', 'confirmedNum'],
-                    0, 0,
-                    1, 1
-                ],
-                // 全局控制热力权重
-                'heatmap-intensity': 1 || [
-                    'interpolate',
-                    ['linear'],
-                    ['zoom'],
-                    0,
-                    1,
-                    9,
-                    3
-                ],
-                // 热力图配色范围
-                'heatmap-color': [
-                    'interpolate',
-                    ['linear'],
-                    ['heatmap-density'],
-                    0,
-                    'rgba(33,102,172,0)',
-                    0.2,
-                    'rgb(103,169,207)',
-                    0.4,
-                    'rgb(209,229,240)',
-                    0.6,
-                    'rgb(253,219,199)',
-                    0.8,
-                    'rgb(239,138,98)',
-                    1,
-                    'rgb(178,24,43)'
-                ],
-                // 每个热力点的绘制半径
-                'heatmap-radius': 10 || [
-                    'interpolate',
-                    ['linear'],
-                    ['zoom'],
-                    0,
-                    2,
-                    9,
-                    20
-                ],
-                // 热力图的透明度
-                'heatmap-opacity': 1 || [
-                    'interpolate',
-                    ['linear'],
-                    ['zoom'],
-                    7,
-                    1,
-                    9,
-                    0
-                ]
-            }
-        },
-        'waterway-label'
-    );
+    // 地区多边形
+    map.addSource('area-polygon', {
+        'type': 'vector',
+        'url': 'mapbox://zhuwenlong.bpyitk7f'
+    });
+
+    map.addLayer({
+        'id': 'area-fill',
+        'source': 'area-polygon',
+        'source-layer': 'cn_sheng_polygon',
+        'type': 'fill',
+        "paint": {
+            "fill-color": '#808080'
+        }
+    })
+
+    map.addLayer({
+        'id': 'area-outline',
+        'source': 'area-polygon',
+        'source-layer': 'cn_sheng_polygon',
+        'type': 'line',
+        "paint": {
+            "line-color": 'rgba(0,0,0,0.3)',
+        }
+    })
 
 
     map.on('click', (e) => {
